54f56731f9da8f72be42ed04b2a134fcb545b28d
Miguel Lopez <ing.miguellv@gmail.com>
2024-05-23 09:33:33 -0600

    fin seccion middlewares

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface Bear {
   id: number;
@@ -12,50 +13,53 @@ interface BearState {
 
   bears: Bear[];
 
-  computed: {
-    totalBears: number;
-  };
-
   increaseBlackBears: (by: number) => void;
   increasePolarBears: (by: number) => void;
   increasePandaBears: (by: number) => void;
+  totalBears: () => number;
 
   doNothing: () => void;
   addBear: () => void;
   clearBears: () => void;
 }
 
-export const useBearStore = create<BearState>()((set, get) => ({
-  blackBears: 10,
-  polarBears: 5,
-  pandaBears: 1,
+export const useBearStore = create<BearState>()(
+  persist(
+    (set, get) => ({
+      blackBears: 10,
+      polarBears: 5,
+      pandaBears: 1,
 
-  bears: [{ id: 1, name: 'Oso #1' }],
+      bears: [{ id: 1, name: 'Oso #1' }],
 
-  computed: {
-    get totalBears() {
-      return (
-        get().blackBears +
-        get().polarBears +
-        get().pandaBears +
-        get().bears.length
-      );
-    },
-  },
+      totalBears: () => {
+        return (
+          get().blackBears +
+          get().polarBears +
+          get().pandaBears +
+          get().bears.length
+        );
+      },
 
-  increaseBlackBears: (by: number) =>
-    set((state) => ({ blackBears: state.blackBears + by })),
-  increasePolarBears: (by: number) =>
-    set((state) => ({ polarBears: state.polarBears + by })),
-  increasePandaBears: (by: number) =>
-    set((state) => ({ pandaBears: state.pandaBears + by })),
-  doNothing: () => set((state) => ({ bears: [...state.bears] })),
-  addBear: () =>
-    set((state) => ({
-      bears: [
-        ...state.bears,
-        { id: state.bears.length + 1, name: `Oso #${state.bears.length + 1}` },
-      ],
-    })),
-  clearBears: () => set({ bears: [] }),
-}));
+      increaseBlackBears: (by: number) =>
+        set((state) => ({ blackBears: state.blackBears + by })),
+      increasePolarBears: (by: number) =>
+        set((state) => ({ polarBears: state.polarBears + by })),
+      increasePandaBears: (by: number) =>
+        set((state) => ({ pandaBears: state.pandaBears + by })),
+      doNothing: () => set((state) => ({ bears: [...state.bears] })),
+      addBear: () =>
+        set((state) => ({
+          bears: [
+            ...state.bears,
+            {
+              id: state.bears.length + 1,
+              name: `Oso #${state.bears.length + 1}`,
+            },
+          ],
+        })),
+      clearBears: () => set({ bears: [] }),
+    }),
+    { name: 'bears-store' }
+  )
+);
